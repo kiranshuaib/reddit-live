d19ad7015984916e382422f93436f6b38efb48dc
kiranshuaib <kiran.shuaib@gmail.com>
2018-07-31 12:07:59 -0400

    add entire app

---
+++ src/Reddit.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class Reddit extends React.Component {
+	state = {
+		posts: [],
+	};
+
+	componentDidMount() {
+		axios.get(`https://www.reddit.com/r/reactjs.json`).then(res => {
+			const posts = res.data.data.children.map(obj => obj.data);
+			this.setState({ posts });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>/r/reactjs</h1>
+				<ul>
+					{this.state.posts.map(post => {
+						return <li key={post.id}>{post.title}</li>;
+					})}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Reddit;
